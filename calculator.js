2729f33647300dde6d1498ab3604d3b3819e6fe3
rm-888 <138863099+rm-888@users.noreply.github.com>
2024-04-25 22:11:48 +0530

    Delete calculator.js

--- calculator.js
+++
@@ -1,8 +0,0 @@
-function toggleCalculator() {
-   var calculatorPopup = document.getElementById('calculatorPopup');
-   if (calculatorPopup.style.display === 'none' || calculatorPopup.style.display === '') {
-      calculatorPopup.style.display = 'block';
-   } else {
-      calculatorPopup.style.display = 'none';
-   }
-}
